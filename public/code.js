2edf1d04440cf91a214aecbe4a6b8db3484c08c6
mani agah esmaeilzad <74871543+mani-agah-esmaeilzad@users.noreply.github.com>
2024-04-01 02:32:15 +0330

    add all of the actions

--- public/code.js
+++ public/code.js
@@ -32,12 +32,32 @@
     function renderMessage(type, message) {
         let messageContainer = app.querySelector(".chat-screen .messages");
         if (type == "my") {
-
+            let el = document.createElement("div");
+            el.setAttribute("class", "message my-message")
+            el.innerHTML = `
+                <div>
+                    <div class="name">You</div>
+                    <div class="text">${message.text}</div>
+                </div>
+            `;
+            messageContainer.appendChild(el);
         } else if (type == "other") {
+            let el = document.createElement("div");
+            el.setAttribute("class", "message other-message")
+            el.innerHTML = `
+                <div>
+                    <div class="name">${message.username}</div>
+                    <div class="text">${message.text}</div>
+                </div>
+            `;
+            messageContainer.appendChild(el);
 
-        } else if (type == "update")
-        {
-            
+        } else if (type == "update") {
+            let el = document.createElement("div");
+            el.setAttribute("class", "update")
+            el.innerText = message;
+            messageContainer.appendChild(el);
         }
+        
     }
 })();
