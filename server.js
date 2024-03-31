6a9c4de8d89554dfc2a6e086bdfdf0a901694861
mani agah esmaeilzad <74871543+mani-agah-esmaeilzad@users.noreply.github.com>
2024-04-01 02:10:16 +0330

    add  io on and off

--- server.js
+++ server.js
@@ -4,6 +4,18 @@ const path = require("path");
 const app = express();
 const server = require("http").createServer(app);
 
-app.use(express.static(path.join(__dirname+"/public")));
+const io = require("socket.io")(server);
+app.use(express.static(path.join(__dirname + "/public")));
 
+io.on("connection", function (socket) {
+  socket.on("newuser", function (username) {
+    socket.broadcast.emit("update", username + "joined in the conversation");
+  });
+  socket.on("exituser", function (username) {
+    socket.broadcast.emit("update", username + "left in the conversation");
+  });
+  socket.on("chat", function (message) {
+    socket.broadcast.emit("chat", message);
+  });
+});
 server.listen(3000);
