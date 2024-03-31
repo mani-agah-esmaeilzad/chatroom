0d5af2444c64eb380257da6f112d22a3569ebb1f
mani agah esmaeilzad <74871543+mani-agah-esmaeilzad@users.noreply.github.com>
2024-04-01 02:00:23 +0330

    add listen in server

--- server.js
+++ server.js
@@ -4,4 +4,8 @@ const path = require("path");
 const app = express();
 const server = require("http").createServer(app);
 
-app.use(express.static)
+app.use(express.static(path.join(__dirname+"/public")));
+
+server.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+  });
